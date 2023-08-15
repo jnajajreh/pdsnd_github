106fedb206dc6cfd483f9fccd383553115bacb3f
jNajajreh@bethlehem.edu <jnajajreh@bethlehem.edu>
2023-08-15 11:56:36 +0300

    Update documentation in bikeshare file one more time

--- BikeshareUS.py
+++ BikeshareUS.py
@@ -5,3 +5,4 @@ import numpy as np
 # this is another comments from refactoring  branch 
 # this is another comments from refactoring branch 
 # this is bikeshare py file that contains some code 
+# another changes in bikeshare 
