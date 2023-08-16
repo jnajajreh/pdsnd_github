06cea8395843ae8e3f81bed4ad988c37232c678e
jNajajreh@bethlehem.edu <jnajajreh@bethlehem.edu>
2023-08-16 08:02:03 +0300

    refactor: Restructure the Bikeshare file

--- BikeshareUS.py
+++ BikeshareUS.py
@@ -7,3 +7,11 @@ import numpy as np
 # this is bikeshare py file that contains some code 
 # another changes in bikeshare 
 # A new feactures have been added to Bikeshare
+
+
+CITY_DATA = {'chicago': 'chicago.csv', 'new york city': 'new_york_city.csv','washington': 'washington.csv'}
+
+months = ('january', 'february', 'march', 'april', 'may', 'june')
+
+weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday')
+
